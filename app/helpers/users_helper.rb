3a589ac15970076144926e3d3f763f4b63073e84
Kazuhisa Tanabe <fieldpotsec@gmail.com>
2019-08-21 08:10:14 +0900

    Finish user signup

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,8 @@
 module UsersHelper
+
+    def gravatar_for(user, size: 80)
+        gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+        gravarar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+        image_tag(gravarar_url, alt: user.name, class: "gravatar")
+    end
 end
